cf9d34e862343b60da5ac278eea05b391be97d2e
Matthew Kataryniak <Matthew Kataryniak>
2016-12-02 13:08:52 -0500

    Included the missing files from the last commit

---
+++ throwup.c
@@ -0,0 +1,47 @@
+// throwup.c
+// God, I hope this works too
+
+#include "g_local.h"
+#include "throwup.h"
+
+void ThrowUpNow(edict_t *self)
+{
+	vec3_t forward, right, mouth_pos, spew_vector;
+	float rambo;
+	int i;
+
+	// Set the spew vector based on the client's (the player's) view angle
+	AngleVectors(self->client->v_angle, forward, right, NULL);
+
+	// Make the spew start from the mouth
+	VectorScale(forward, 24, mouth_pos);
+	VectorAdd(mouth_pos, self->s.origin, mouth_pos);
+	mouth_pos[2] += self->viewheight;
+
+	// Make the spew come forward from the mouth
+	VectorScale(forward, 24, spew_vector);
+
+	// Make the BLOOD particle effect
+	gi.WriteByte(svc_temp_entity);
+	gi.WriteByte(TE_BLOOD);
+	gi.WritePosition(mouth_pos);
+	gi.WriteDir(spew_vector);
+	gi.multicast(mouth_pos, MULTICAST_PVS);
+
+	//============================
+	// Bonus stuff the tut showed
+	//============================
+
+	// Write out to the console
+	rambo = random();
+	if (rambo < 0.2)
+		gi.bprintf(PRINT_MEDIUM, "Hurgle blurgle\n");
+	else if (rambo < 0.4)
+		gi.bprintf(PRINT_MEDIUM, "...BLARHF...\n");
+	else if (rambo < 0.6)
+		gi.bprintf(PRINT_MEDIUM, "*vomit*\n");
+	else if (rambo < 0.8)
+		gi.bprintf(PRINT_MEDIUM, "There goes lunch...\n");
+	else
+		gi.bprintf(PRINT_MEDIUM, "BBLLZSZLSDSDFLLAARRFFFF\n");
+}
---
+++ throwup.h
@@ -0,0 +1,5 @@
+// throwup.h
+// God I hope this works
+
+// main function
+void ThrowUpNow(edict_t *self);
